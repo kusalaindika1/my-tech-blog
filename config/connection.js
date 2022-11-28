2bd6de0ac4286a8c4c36be79f7c631d42c1e7920
ethugalge liyanage <mycanada123@gmail.com>
2022-11-28 13:59:42 -0500

    added db provisioining

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,21 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: "localhost",
-    dialect: "mysql",
-    port: 3310,
-  }
-);
+// IF we have a HEROKU connection
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  // IF we have a local connection
+  const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3310,
+    }
+  );
+}
 
 module.exports = sequelize;
